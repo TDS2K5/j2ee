42c29e831bc4af2e389cae9b99a1fe965455d70d
TDS2K5 <berlinryuk@gmail.com>
2026-02-21 07:15:33 +0530

    updated

---
+++ CollegeEvents/src/collegeevents/CollegeEvents.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package collegeevents;
+import java.util.*;
+
+enum EventType{
+    WORKSHOP(250,2),HACKATHON(1000,5),QUIZ(500,4),PAPER_PRESENTATION(200,3);
+    float fees;
+    int max_size;
+    
+    EventType(float f, int m){
+        System.out.println("Enter event name: ");
+        Scanner sc=new Scanner(System.in);
+        String e=sc.next().toUpperCase();
+        
+    }
+    void getData(){
+        
+    }
+}
+
+public class CollegeEvents {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        
+        
+        
+    }
+    
+}
